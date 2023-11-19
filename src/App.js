3d514791df83a26be0bdd15cd4e9ea5d5ab15f11
Dev-Golmohammadi <92379327+DEVMGO@users.noreply.github.com>
2023-11-19 12:34:35 +0330

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(null)
+  const [windowHeight, setWindowHeight] = useState(null)
+  useEffect(() => {
+    function handleResize() {
+      const width = window.innerWidth;
+      setWindowWidth(width);
+      const height = window.innerHeight;
+      setWindowHeight(height);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="width">
+        <div className="line">
+          <div className="w-left"></div>
+          <div className="w-line"></div>
+          <div className="w-right"></div>
+        </div>
+        <div className="data">
+          <h5>width: {windowWidth}</h5>
+        </div>
+      </div>
+      <div className="height">
+        <div className="line">
+          <div className="h-top"></div>
+          <div className="h-line"></div>
+          <div className="h-down"></div>
+        </div>
+        <div className="data">
+          <h5>Height: {windowHeight}</h5>
+        </div>
+      </div>
     </div>
   );
 }
